6daec8b7465bb11f640ad0636cd72d63b9fa8783
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-08-14 13:46:26 -0500

    isPalindrome

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 export function isPalindrome(num) {
-  return true;
+  let is = true;
+  const numString = num + '';
+  const length = numString.length;
+  const middle = Math.floor(length / 2);
+
+  for (let i = 0; is && i < middle; i++) {
+    is = numString[i] === numString[length - 1 - i];
+  }
+
+  return is;
 }
--- test/index.js
+++ test/index.js
@@ -2,6 +2,22 @@ import { expect } from 'chai';
 import * as app from '../src';
 
 describe('isPalindrome', () => {
+  describe('3', () => {
+    it('should return true', () => {
+      const expected = true;
+      const actual = app.isPalindrome(3);
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe('66', () => {
+    it('should return true', () => {
+      const expected = true;
+      const actual = app.isPalindrome(66);
+      expect(actual).to.equal(expected);
+    });
+  });
+
   describe('123321', () => {
     it('should return true', () => {
       const expected = true;
@@ -11,9 +27,25 @@ describe('isPalindrome', () => {
   });
 
   describe('12321', () => {
+    it('should return true', () => {
+      const expected = true;
+      const actual = app.isPalindrome(12321);
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe('123221', () => {
     it('should return false', () => {
       const expected = false;
-      const actual = app.isPalindrome(123321);
+      const actual = app.isPalindrome(123221);
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe('12', () => {
+    it('should return false', () => {
+      const expected = false;
+      const actual = app.isPalindrome(12);
       expect(actual).to.equal(expected);
     });
   });
